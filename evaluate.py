2425c22d4f91bbba207f0a4864f306d0e3b0e974
ubuntu <ubuntu@packer-ubuntu-16.04-desktop-gpu>
2018-01-31 20:36:17 +0100

    add no aggregation feature extraction

--- evaluate.py
+++ evaluate.py
@@ -21,9 +21,9 @@ def evaluate(model, data, params):
     # only for string labels
     le = LabelEncoder().fit(data['y']['tg'][:])
     lb = LabelBinarizer().fit(data['y']['tg'][:])
-    id_hash = {v:k for k, v in enumerate(data['ids'][:])}
+    id_hash = {v: k for k, v in enumerate(data['ids'][:])}
     val_ids = sorted(shuffle_ids(params['split']['valid'], id_hash))
-    
+
     Ypr = []
     for i in trange(0, len(val_ids), params['batch_sz'], ncols=80):
         X = data['X'][val_ids[i:i+params['batch_sz']]]
@@ -36,7 +36,7 @@ def evaluate(model, data, params):
         Ypr.append(np.array(pred).mean(axis=0))
     Ypr = np.concatenate(Ypr, axis=0)
     Y = lb.transform(data['y']['tg'][val_ids])
-    
+
     y, ypr = np.argmax(Y, axis=1), np.argmax(Ypr, axis=1)
     y_label = le.inverse_transform(y)
     ypr_label = le.inverse_transform(ypr)
@@ -68,8 +68,6 @@ def test(model, data, train_id, test_fn, params):
                     pred.append(model['tg']['predict'](x))
             Ypr.append(np.array(pred).mean(axis=0))
         Ypr = np.concatenate(Ypr, axis=0)
-        ypr = np.argmax(Ypr, axis=1)
-        ypr_label = le.inverse_transform(ypr)
 
         out_df = pd.DataFrame(
             Ypr, columns=le.classes_, index=test_data['ids'][:])
@@ -78,28 +76,64 @@ def test(model, data, train_id, test_fn, params):
         out_df.to_csv('results/{}.csv'.format(train_id))
 
 
-def feature(model, data, params):
+def feature(model, data, params,
+            target='tg', agg=[np.mean, np.std, np.median]):
     """"""
-    N = data['X'].shape[0]
+    id_hash = {v: k for k, v in enumerate(data['ids'][:])}
+    trn_ids = [(id_hash[int(i)], 'train') for i in params['split']['train']
+               if int(i) in id_hash]
+    val_ids = [(id_hash[int(i)], 'valid') for i in params['split']['valid']
+               if int(i) in id_hash]
+    ix_dset_map = dict(sorted(trn_ids + val_ids, key=lambda x: x[0]))
+
+    # N = data['X'].shape[0]
+    N = len(ix_dset_map)
+    bs = params['batch_sz']
     Z = []  # feature
-    for i in trange(0, N, params['batch_sz'], ncols=80):
-        X = data['X'][i:i+params['batch_sz']]
-        M = data['mask'][i:i+params['batch_sz']]
+    Y = []  # label
+    DSET = []  # set identifier
+    IDS = []
+    for i in trange(0, N, bs, ncols=80):
+        slc = slice(i, i + bs)
+        if i + bs > N:
+            ids = range(i, N)
+        else:
+            ids = range(i, i + bs)
+        dset = map(lambda j: ix_dset_map[j], ids)
+        X = data['X'][slc]
+        M = data['mask'][slc]
+        y = data['y']['tg'][slc]
         feat = []
         for j in range(0, X.shape[-2], params['dur'] / 2):
             x = X[:, :, j:j+params['dur']]
             if x.shape[-2] >= params['dur']:
-                feat.append(model['tg']['transform'](x))
+                feat.append(model[target]['transform'](x))
         feat = np.array(feat)
-        Z.append(
-            np.concatenate([
-                feat.mean(axis=0),
-                feat.std(axis=0),
-                np.median(feat, axis=0)
-            ], axis=-1)
-        )
+        if isinstance(agg, list):
+            F = np.concatenate([
+                stat(feat, axis=0) for stat in agg],
+                axis=-1
+            )
+            Y.append(y)
+            DSET.extend(dset)
+            IDS.extend(ids)
+        elif agg is None:
+            # now it's (batch * chunk, feat)
+            Y.append(np.repeat(y, feat.shape[0]))
+            DSET.append(np.repeat(dset, feat.shape[0]))
+            IDS.append(np.repeat(ids, feat.shape[0]))
+            F = np.concatenate(feat.transpose(1, 0, 2))
+        else:
+            raise Exception
+        Z.append(F)
+
+    if agg is None:
+        Y = np.concatenate(Y)
+        DSET = np.concatenate(DSET)
+        IDS = np.concatenate(IDS)
+
     Z = np.concatenate(Z, axis=0)
-    return Z
+    return Z, Y, DSET, IDS
 
 
 def prepare_submission(train_id, test_fn, path='results/'):
--- feature_ext.py
+++ feature_ext.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 from utils import load_check_point
 from evaluate import feature
 import h5py
@@ -6,18 +7,19 @@ from sklearn.externals import joblib
 import fire
 
 
-def feature_ext(train_id, data_fn, out_path, model_path='results/'):
-	""""""
-	print 'Extracting!...'
-	with h5py.File(data_fn, 'r') as hf:
-		net, mdl, params = load_check_point(train_id, path=model_path)
-		Z = feature(mdl, hf, params)  # feature
-		y = hf['y']['tg'][:]
-		
-	print 'Saving output...'
-	out_fn = os.path.join(out_path, train_id + '.dat.gz')
-	joblib.dump((Z, y), out_fn)
+def feature_ext(train_id, data_fn, out_path,
+                agg=[np.mean, np.std, np.median, np.amin, np.amax],
+                target='tg', model_path='results/'):
+    """"""
+    print 'Extracting!...'
+    with h5py.File(data_fn, 'r') as hf:
+        net, mdl, params = load_check_point(train_id, path=model_path)
+        Z, y, dset, ids = feature(mdl, hf, params, target, agg)  # feature
+
+    print 'Saving output...'
+    out_fn = os.path.join(out_path, train_id + '.dat.gz')
+    joblib.dump((Z, y, dset, ids), out_fn)
 
 
 if __name__ == "__main__":
-	fire.Fire(feature_ext)
+    fire.Fire(feature_ext)
